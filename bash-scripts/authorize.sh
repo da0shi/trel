d4a562c3c7b0ae602937ee8fb4fbbfea771c60d3
da0shi <whit3.develop@gmail.com>
2015-05-14 09:58:42 +0900

    Create url to generate token

--- bash-scripts/authorize.sh
+++ bash-scripts/authorize.sh
@@ -1,5 +1,23 @@
 #!/bin/sh
 
-authorize_url="https://trello.com/1/authorize"
+mkQuery () {
+	result=$1
+	shift 1
+	for param in $@
+	do
+		result="${result}&${param}"
+	done
+	echo ${result}
+}
 
+authorize_url="https://trello.com/1/authorize"
+name="name=trel"
+expiration="expiration=1day"
+response_type="response_type=token"
+scope="scope=read"
+key="key="
+
+query=`mkQuery ${name} ${expiration} ${response_type} ${scope} ${key}`
+
+echo "${authorize_url}?${query}"
 
